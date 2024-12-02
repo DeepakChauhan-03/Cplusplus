bd550c8db685e70b83bdbb07bcb643ea8c7a1319
Deepak Chauhan <deepakchauhan6141@gmail.com>
2024-12-02 22:30:19 +0530

    Op overloading

--- OOPs/Polymorphism/OperatorOverloading.cpp
+++ OOPs/Polymorphism/OperatorOverloading.cpp
@@ -1,1 +1,29 @@
 //Operator Overloading
+#include<iostream>
+using namespace std;
+class Space{
+    public:
+        int x;
+        int y;
+        int z;
+    Space(int x,int y,int z){   //Constructor
+        this->x=x;
+        this->y=y;
+        this->z=z;
+    }    
+    void showData(){
+        cout<<"\n(x,y,z) : ("<<x<<","<<y<<","<<z<<")";
+    }
+    void operator -(){
+        x = -x;
+        y = -y;
+        z = -z;
+    }
+};
+int main(){
+    Space s(10,-20,30);
+    s.showData();
+    -s;
+    s.showData();
+    return 0;
+}
