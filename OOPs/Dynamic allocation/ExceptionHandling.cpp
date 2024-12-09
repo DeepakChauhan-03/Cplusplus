7eaaf96b0746689a17392c43d72a86ff118f1345
Deepak Chauhan <deepakchauhan6141@gmail.com>
2024-12-09 20:59:31 +0530

    Exception

---
+++ OOPs/Dynamic allocation/ExceptionHandling.cpp
@@ -0,0 +1,35 @@
+//Exception handling
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Line 1 "<<endl;
+    cout<<"Line 2 "<<endl;
+    cout<<"Line 3 "<<endl;
+    int a=20,b=1,c=0;
+
+    try{
+        cout<<"\nInside Try block";
+        if(b==0){
+            throw b;
+        }
+        if(b>1){
+            throw 2.0;
+        }
+        c=a/b;
+        cout<<"\nThis is catch block";
+    }
+    catch(int i){
+        cout<<"\nDenominator should not be "<<i<<endl;
+    }
+    catch(float i){
+        cout<<"\nCatch block float executed "<<i;
+    }
+    
+    cout<<"Line 4 "<<endl;
+    cout<<"Line 5 "<<endl;
+    cout<<"Line 6 "<<endl;
+    cout<<"Line 7 "<<endl;
+    cout<<"Line 8 "<<endl;
+
+    return 0;
+}
